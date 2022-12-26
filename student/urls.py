4fe1e6202cb366283f14ab0d1e73c01006190df1
Shaili <shailimaurya945@gmail.com>
2022-12-26 11:31:34 +0530

    initial commit

---
+++ student/urls.py
@@ -0,0 +1,12 @@
+# from django.urls import path
+
+from django.urls import path
+from .views import signup,login,logout
+
+urlpatterns = [
+    path('signup/', signup),
+    path('login/', login),
+    path('logout/', logout),
+   
+
+]
